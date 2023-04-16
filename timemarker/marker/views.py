cd2a19e1e3b32aff87c2cfa159ee3336acdd1339
lvsdvale <vale@alunos.utfpr.edu.br>
2023-04-16 16:18:03 -0300

    feature:settings update

--- timemarker/marker/views.py
+++ timemarker/marker/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView,ListView
+from django.contrib import messages
+from django.db import models
+from marker.models import *
 
 # Create your views here.
+class SubtaskView(ListView):
+    template_name = 'index.html'
+    context_object_name = 'index'
+    paginate_by = 5
+
+    def get_queryset(self):
+        queryset = SubTaskModel.objects.all()
+        q = self.request.GET.get('q', '')
+        if q:
+            queryset = queryset.filter(
+                models.Q(name__icontains=q) | models.Q(descricao__icontains=q)
+            )
+
+        return queryset
